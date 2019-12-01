27eec91f3fc80b6fb3ab03cabb2c7f2026463392
MercuriusXeno <mercuriusxeno@gmail.com>
2019-12-01 16:05:30 -0600

    Adding filler models
    Fixing missing hopper models and adding "unreachable" models for interloper to dodge some dumb errors.

--- src/main/java/com/mercuriusxeno/mercurialtools/block/ModBlocks.java
+++ src/main/java/com/mercuriusxeno/mercurialtools/block/ModBlocks.java
@@ -5,18 +5,25 @@ import net.minecraftforge.registries.ObjectHolder;
 public class ModBlocks {
     @ObjectHolder("mercurialtools:condensing_hopper")
     public static CondensingHopper CONDENSING_HOPPER;
+
     @ObjectHolder("mercurialtools:ender_keystone")
     public static EnderKeystone ENDER_KEYSTONE;
+
     @ObjectHolder("mercurialtools:ender_vacuum")
     public static EnderVacuum ENDER_VACUUM;
+
     @ObjectHolder("mercurialtools:enticing_prism")
     public static EnticingPrism ENTICING_PRISM;
+
     @ObjectHolder("mercurialtools:expanding_hopper")
     public static ExpandingHopper EXPANDING_HOPPER;
+
     @ObjectHolder("mercurialtools:growth_pusler")
     public static GrowthPulser GROWTH_PULSER;
+
     @ObjectHolder("mercurialtools:interloper")
     public static Interloper INTERLOPER;
+
     @ObjectHolder("mercurialtools:spawner_template")
     public static SpawnerTemplate SPAWNER_TEMPLATE;
 }
